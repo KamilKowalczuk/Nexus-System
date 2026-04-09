e416b73a3459c8f9dd6f84ccc9dcd42e02a8b013
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2026-04-09 18:24:36 +0200

    Wersja produkcyjna

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 uv run uvicorn api:app --host 0.0.0.0 --port 8000 &
 PID_API=$!
 
-# Uruchamiamy Streamlit na pierwszym planie (tymczasowo aż nowy panel przejmie 100% funkcji)
-uv run streamlit run gui/dashboard.py --server.port=8501 --server.address=0.0.0.0
+# # Uruchamiamy Streamlit na pierwszym planie (tymczasowo aż nowy panel przejmie 100% funkcji)
+# uv run streamlit run gui/dashboard.py --server.port=8501 --server.address=0.0.0.0
 
 # Oczekujemy na proces w tle w razie błędu
 wait $PID_API
