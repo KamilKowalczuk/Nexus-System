696c87751e3df4d7455f33d00673bff1464fb5f8
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2026-04-11 19:13:13 +0200

    scout

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
-# #!/bin/bash
+#!/bin/bash
+set -e
 
 echo "🚀 Uruchamiam Nexus API (FastAPI) na porcie 8000..."
-uv run uvicorn api:app --host 0.0.0.0 --port 8000 &
-
-# Czekamy na wszystkie procesy w tle, aby kontener nie zakończył pracy
-wait
+exec uv run uvicorn api:app --host 0.0.0.0 --port 8000
